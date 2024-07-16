730d37805afa7135593d82ad9182dbe131defddf
ftyelbi <ftyelbi@gmail.com>
2024-07-16 09:02:29 +0000

    App.js updated

--- MFA-App/mfa-mern-app/src/App.js
+++ MFA-App/mfa-mern-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+mport logo from './logo.svg';
 import './App.css';
+import HelloWorld from './HelloWorld'; // Import the new component
 
 function App() {
-  return (
-    <div className="mfa-App">
-      <header className="mfa-App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	  return (
+		      <div className="mfa-App">
+		        <header className="mfa-App-header">
+		          <img src={logo} className="App-logo" alt="logo" />
+		          <p>Welcome to My App!</p>
+		          <a
+		            className="App-link"
+		            href="(link unavailable)"
+		            target="_blank"
+		            rel="noopener noreferrer"
+		          >
+		            Learn React
+		          </a>
+		        </header>
+		        <HelloWorld /> // Add the new component here
+		      </div>
+		    );
 }
 
 export default App;
